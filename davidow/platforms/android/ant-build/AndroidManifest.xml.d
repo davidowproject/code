00285cff9344a8cd3f9d4c0d0a809b6d02462225
Judy <judynguyen90@gmail.com>
2015-09-17 14:56:35 -0500

    build

--- davidow/platforms/android/ant-build/AndroidManifest.xml.d
+++ davidow/platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\Users\Dylan\Documents\GitHub\code\davidow\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Dylan\Documents\GitHub\code\davidow\platforms\android\AndroidManifest.xml \
+C:\Users\Judy\Documents\GitHub\code\davidow\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Judy\Documents\GitHub\code\davidow\platforms\android\AndroidManifest.xml \
