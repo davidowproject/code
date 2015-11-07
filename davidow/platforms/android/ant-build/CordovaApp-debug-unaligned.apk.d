29e736877ab015ab8f10da499b18ea6996f46933
Dylan Orgeron <dylan.orgeron@gmail.com>
2015-11-06 23:24:05 -0600

    menu wip

--- davidow/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++ davidow/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -1,5 +1,5 @@
-C:\Users\Dylan-PC\Documents\GitHub\code\davidow\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
-C:\Users\Dylan-PC\Documents\GitHub\code\davidow\platforms\android\ant-build\CordovaApp.ap_ \
-C:\Users\Dylan-PC\Documents\GitHub\code\davidow\platforms\android\ant-build\classes.dex \
-C:\Users\Dylan-PC\Documents\GitHub\code\davidow\platforms\android\CordovaLib\ant-build\classes.jar \
-C:\Users\Dylan-PC\Documents\GitHub\code\davidow\platforms\android\libs\estimote-sdk-preview.jar \
+C:\Users\Dylan\Documents\GitHub\code\davidow\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\Users\Dylan\Documents\GitHub\code\davidow\platforms\android\ant-build\CordovaApp.ap_ \
+C:\Users\Dylan\Documents\GitHub\code\davidow\platforms\android\ant-build\classes.dex \
+C:\Users\Dylan\Documents\GitHub\code\davidow\platforms\android\CordovaLib\ant-build\classes.jar \
+C:\Users\Dylan\Documents\GitHub\code\davidow\platforms\android\libs\estimote-sdk-preview.jar \
