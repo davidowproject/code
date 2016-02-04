82aed6045c405d87e6028c40811f5074655671b2
Dylan Orgeron <dylan.orgeron@gmail.com>
2016-02-04 12:39:08 -0600

    solved the flicker

--- davidow/www/js/ranging.js
+++ davidow/www/js/ranging.js
@@ -2,50 +2,50 @@ var shownBeacons = []
 /*
 {
 	id: 0,
-	timer: 0
+	lastFound: 0
 }
 */
 
-var onRange = function(beacons){
-	//reset dom
-	$(".tray-item").hide()
-	//beacons come in as array so we sort through them
-	$.each(beacons, function(i,beacon){
-		//store id for ease of access
-		var id = beacon.id
-		//check array of beacons to see if its already shown
+var onRange = function(foundBeacons){
+	if(shownBeacons.length){
+		$.each(shownBeacons, function(i, shownBeacon){
+			$.each(foundBeacons, function(j, foundBeacon){
+				if(shownBeacon.id == foundBeacon.id){
+					shownBeacon.lastFound = Date.now()
+				}else if(Date.now() - shownBeacon.lastFound > 10000){
+					hideBeacon(shownBeacon.id)
+					shownBeacons.splice(i,1)
+				}
+			})
+		})
+	}
+	$.each(foundBeacons,function(i, foundBeacon){
 		var found = 0
-		$.each(shownBeacons, function(i,v){
-			if(id == v.id){
-				v.timer = Date.now()
+		$.each(shownBeacons, function(j, shownBeacon){
+			if(foundBeacon.id == shownBeacon.id){
 				found = 1
 			}
 		})
-		/*
-		we went thru the list of shown beacons and this beacon wasnt there.
-		this means that we have a found a new beacon and we need to show it
-		in the dom and add it to shown beacons.
-		*/
 		if(!found){
-			//make dom changes
-			showBeacon(id)
-			//update shownBeacons with current id and timestamp
-			shownBeacons.push({
-				id: id,
-				timer: Date.now()
-			})
+			showBeacon(foundBeacon.id)
+			shownBeacons.push({id: foundBeacon.id, lastFound: Date.now()})
 		}
-	})//end beacon loop
+	})
+}
+
+
+var hideBeacon = function(id){
+	$.each(pieces, function(i,v){
+		if(v.beacon_id == id){
+			$("#"+v.id).parent().hide()
+		}
+	})
 }
 
 var showBeacon = function(id){
-	//logic for showing beacon in dom
 	$.each(pieces, function(i,v){
 		if(v.beacon_id == id){
-			//show the piece's bar in the list
 			$("#"+v.id).parent().show()
-			//show the piece on the map
-			
 		}
 	})
 }
