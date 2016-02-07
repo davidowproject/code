3720cb657b3e6e2cf2dd9bdc1a9f9dd8a7c95116
Dylan Orgeron <dylan.orgeron@gmail.com>
2016-02-07 11:30:56 -0600

    housekeeping

--- davidow/www/js/ranging.js
+++ davidow/www/js/ranging.js
@@ -5,47 +5,47 @@ var shownBeacons = []
 	lastFound: 0
 }
 */
-
 var onRange = function(foundBeacons){
-	if(shownBeacons.length){
-		$.each(shownBeacons, function(i, shownBeacon){
-			$.each(foundBeacons, function(j, foundBeacon){
-				if(shownBeacon.id == foundBeacon.id){
-					shownBeacon.lastFound = Date.now()
-				}else if(Date.now() - shownBeacon.lastFound > 10000){
-					hideBeacon(shownBeacon.id)
-					shownBeacons.splice(i,1)
+	if(foundBeacons.length){
+		if(shownBeacons.length){
+			$.each(shownBeacons, function(i, shownBeacon){
+				$.each(foundBeacons, function(j, foundBeacon){
+					if(shownBeacon.id == foundBeacon.id){
+						shownBeacon.lastFound = Date.now()
+					}else if(Date.now() - shownBeacon.lastFound > 10000){
+						hideBeacon(shownBeacon.id)
+						shownBeacons.splice(i,1)
+					}
+				})
+			})
+		}
+		$.each(foundBeacons,function(i, foundBeacon){
+			var found = 0
+			$.each(shownBeacons, function(j, shownBeacon){
+				if(foundBeacon.id == shownBeacon.id){
+					found = 1
 				}
 			})
-		})
-	}
-	$.each(foundBeacons,function(i, foundBeacon){
-		var found = 0
-		$.each(shownBeacons, function(j, shownBeacon){
-			if(foundBeacon.id == shownBeacon.id){
-				found = 1
+			if(!found){
+				showBeacon(foundBeacon.id)
+				shownBeacons.push({id: foundBeacon.id, lastFound: Date.now()})
 			}
 		})
-		if(!found){
-			showBeacon(foundBeacon.id)
-			shownBeacons.push({id: foundBeacon.id, lastFound: Date.now()})
-		}
-	})
+	}else{
+		$(".tray-item").fadeOut()
+	}
 }
-
-
 var hideBeacon = function(id){
 	$.each(pieces, function(i,v){
 		if(v.beacon_id == id){
-			$("#"+v.id).parent().hide()
+			$("#"+v.id).parent().fadeOut()
 		}
 	})
 }
-
 var showBeacon = function(id){
 	$.each(pieces, function(i,v){
 		if(v.beacon_id == id){
-			$("#"+v.id).parent().show()
+			$("#"+v.id).parent().fadeIn()
 		}
 	})
 }
